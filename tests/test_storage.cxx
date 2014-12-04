f1d1862dfee91de4f8e314d5732cf9c7c450ddbe
Garrin McGoldrick <garrin.mcgoldrick@gmail.com>
2014-12-04 17:13:19 -0500

    StorageIO tests

--- tests/test_storage.cxx
+++ tests/test_storage.cxx
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <stdexcept>
 
+#include <TSystem.h>
+
 #include "storage/storageo.h"
 #include "storage/storagei.h"
 #include "storage/storageio.h"
@@ -282,6 +284,51 @@ int test_event() {
   return 0;
 }
 
+int test_plane() {
+  const size_t nplanes = 2;
+  Storage::Event event(nplanes);
+
+  if (event.getPlane(0).getPlaneNum() != 0 || event.getPlane(1).getPlaneNum() != 1) {
+    std::cerr << "Storage::Plane: getPlaneNum() failed" << std::endl;
+    return -1;
+  }
+
+  Storage::Hit& hit0 = event.newHit(0);
+  Storage::Hit& hit1 = event.newHit(1);
+  Storage::Hit& hit2 = event.newHit(0);
+
+  if (hit0.fetchPlane() != &event.getPlane(0) || hit1.fetchPlane() != &event.getPlane(1) || hit2.fetchPlane() != &event.getPlane(0)) {
+    std::cerr << "Storage::Plane: hit plane association failed" << std::endl;
+    return -1;
+  }
+
+  Storage::Cluster& cluster0 = event.newCluster(0);
+  Storage::Cluster& cluster1 = event.newCluster(1);
+  Storage::Cluster& cluster2 = event.newCluster(0);
+
+  if (cluster0.fetchPlane() != &event.getPlane(0) || cluster1.fetchPlane() != &event.getPlane(1) || cluster2.fetchPlane() != &event.getPlane(0)) {
+    std::cerr << "Storage::Plane: cluster plane association failed" << std::endl;
+    return -1;
+  }
+
+  return 0;
+}
+
+int test_storageio() {
+  Storage::StorageIO store("tmp.root", Storage::StorageIO::OUTPUT); 
+
+  if (store.getNumEvents() != 0 ||
+      store.getNumPlanes() != 0 ||
+      store.getFileMode() != Storage::StorageIO::OUTPUT ||
+      store.getMaskMode() != Storage::StorageIO::REMOVE) {
+    std::cerr << "Storage:StorageIO: default values not as expected" << std::endl;
+    return -1;
+  }
+
+  gSystem->Exec("rm -f tmp.root");
+  return 0;
+}
+
 int main() {
   int retval = 0;
 
@@ -290,6 +337,8 @@ int main() {
     if ((retval = test_cluster()) != 0) return retval;
     if ((retval = test_track()) != 0) return retval;
     if ((retval = test_event()) != 0) return retval;
+    if ((retval = test_plane()) != 0) return retval;
+    if ((retval = test_storageio()) != 0) return retval;
   }
   
   catch (std::exception& e) {
